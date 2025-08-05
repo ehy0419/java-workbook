6b12ee9512458b2346c8e5fc4538a7f229879718
ehy0419 <2hy0419@naver.com>
2025-08-05 22:19:58 +0900

    feat:[breaknontinue]Q2 기댓값 1 3 5 7 9 확인

--- src/main/java/workbook1/breakcontinue/Q2.java
+++ src/main/java/workbook1/breakcontinue/Q2.java
@@ -3,6 +3,10 @@ package main.java.workbook1.breakcontinue;
 public class Q2 {
     public static void main(String[] args) {
         // TODO: 1~10 사이의 홀수만 출력하도록 continue 사용
+        for (int num = 1; num <= 10; num++) {       //사고흐름 1. 3.
+            if (num % 2 == 0) continue;             //사고흐름 2.
+            System.out.print(num);                  //사고흐름 4.
+        }
     }
 }
 
