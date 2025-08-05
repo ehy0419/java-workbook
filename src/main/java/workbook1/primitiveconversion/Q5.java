43afb77ee5c83491299b8f2c15b3b31cd1b4556f
ehy0419 <2hy0419@naver.com>
2025-08-06 07:01:22 +0900

    feat:[primitiveconversion]Q5 실수값 입력 받기

--- src/main/java/workbook1/primitiveconversion/Q5.java
+++ src/main/java/workbook1/primitiveconversion/Q5.java
@@ -5,9 +5,18 @@ import java.util.Scanner;
 public class Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double x = sc.nextDouble();
-        double y = sc.nextDouble();
-        double z = sc.nextDouble();
         // TODO: x, y, z를 int로 변환 후 평균을 float으로 출력
+        //1. 실수 값 3개 입력받기
+        System.out.print("실수 x를 입력해주세요: ");
+        double x = sc.nextDouble();
+        System.out.print("실수 y를 입력해주세요: ");
+        double y = sc.nextDouble();
+        System.out.print("실수 z를 입력해주세요: ");
+        double z = sc.nextDouble();
+
     }
 }
+
+/// 사고흐름
+//1. 실수 값 3개 입력받기
+
