3193b0412dd62bfbc2851cffb247931f2073e486
ehy0419 <2hy0419@naver.com>
2025-08-06 06:04:36 +0900

    fix:[primitiveconversion]Q1 버그이슈 설명

--- src/main/java/workbook1/primitiveconversion/Q1.java
+++ src/main/java/workbook1/primitiveconversion/Q1.java
@@ -6,18 +6,22 @@ public class Q1 {
     public static void main(String[] args) {
 
         /// bug 이슈
+        // Scanner 입력을 받아야 결과가 출력되는 것.
+        // 콘솔에 아무것도 안 떠서 무슨 문제가 있나 봤더니 입력 대기 상태였던 것.
+        // 사실 int a = sc.nextInt(); 에서 입력을 대기 했던 것
+
         Scanner sc = new Scanner(System.in);
         // TODO: a와 b를 double로 자동 형변환하여 더한 뒤 int로 강제 변환하여 출력
         /// 1. 사용자로부터 정수 a와 b를 입력받음
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = sc.nextInt(); // 5
+        int b = sc.nextInt(); // 10
 
         ///2. 정수 a와 b를 double로 자동 형변환하여 더함
         double sum = (double)a+(double)b ;
 
         ///3. 변환된 double 타입의 합을 (int)로 강제 형변환하여 출력
         int result = (int)sum;
-        System.out.println(result);
+        System.out.println(result); // 15
     }
 }
 
