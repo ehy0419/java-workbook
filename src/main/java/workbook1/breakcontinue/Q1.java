e8c04e5faf5e616c9a220c17b956ef4ae928be93
ehy0419 <2hy0419@naver.com>
2025-08-05 21:42:47 +0900

    test:[breaknontinue]Q1 기댓값 없음

--- src/main/java/workbook1/breakcontinue/Q1.java
+++ src/main/java/workbook1/breakcontinue/Q1.java
@@ -2,9 +2,17 @@ package main.java.workbook1.breakcontinue;
 
 public class Q1 {
     public static void main(String[] args) {
-        int sum = 0;
         // TODO: 1부터 순차적으로 더하다가 10의 배수를 만나면 break
+        // 누적 합을 저장할 변수 초기화
+        int sum = 0;
+
+        //1. 숫자 num 1부터 1씩 증가하며 100까지 증가
+        for (int num = 1; num <= 100; num++) {
+            //2. 숫자 num은 10의 배수인지 확인
+            if (num%10 == 0) break; //4. 10의 배수를 나오면 종료
+        }
         // 합을 출력
+        System.out.println("10의 배수 이전까지의 합 : " + sum);  // 초기화된 sum 적용
     }
 }
 
