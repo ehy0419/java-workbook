68f26c874ca9a90385849bbf094bbc9353da00e9
ehy0419 <2hy0419@naver.com>
2025-08-06 07:37:10 +0900

    feat:[operators]Q1 완성

--- src/main/java/workbook1/operators/Q1.java
+++ src/main/java/workbook1/operators/Q1.java
@@ -5,8 +5,22 @@ import java.util.Scanner;
 public class Q1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
         // TODO: 산술 연산자(+, -, *, /, %) 결과 출력
+        //1. 두 개의 정수 입력 받기
+        System.out.print("첫 번째 정수를 입력해주세요: ");
+        int a = sc.nextInt();
+        System.out.print("첫 번째 정수를 입력해주세요: ");
+        int b = sc.nextInt();
+
+        //2. 산술 연산자(+, -, *, /, %) 적용하기
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        int div = a / b;
+        int mod = a%b;
+
+        //3.결과는 공백으로 구분해서 출력하기
+        System.out.println("결과: " + sum + " " + diff + " " + mul + " " + div + " " + mod);
+
     }
 }
