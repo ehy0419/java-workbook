8a7b6d44cbd36c6a1cc3e90c09a80f0c41e8928f
ehy0419 <2hy0419@naver.com>
2025-08-06 07:07:04 +0900

    feat:[primitiveconversion]Q5 주석 추가

--- src/main/java/workbook1/primitiveconversion/Q5.java
+++ src/main/java/workbook1/primitiveconversion/Q5.java
@@ -7,24 +7,24 @@ public class Q5 {
         Scanner sc = new Scanner(System.in);
         // TODO: x, y, z를 int로 변환 후 평균을 float으로 출력
         //1. 실수 값 3개 입력받기
-        System.out.print("실수 x를 입력해주세요: ");
+        System.out.print("실수 x를 입력해주세요: "); // 3.8
         double x = sc.nextDouble();
-        System.out.print("실수 y를 입력해주세요: ");
+        System.out.print("실수 y를 입력해주세요: "); // 4.2
         double y = sc.nextDouble();
-        System.out.print("실수 z를 입력해주세요: ");
+        System.out.print("실수 z를 입력해주세요: "); //5.9
         double z = sc.nextDouble();
 
         //2. 실수 값을 int로 정수형으로 강제 변환시키기
-        int ix = (int) x;
-        int iy = (int) y;
-        int iz = (int) z;
+        int ix = (int) x; // 3
+        int iy = (int) y; // 4
+        int iz = (int) z; // 5
 
         //3. 변환된 정수 값의 평균 계산하기 (float 값으로)
-        int sum = ix + iy + iz;
-        float avg = (float) sum / 3;
+        int sum = ix + iy + iz;  // 3 + 4 + 5
+        float avg = (float) sum / 3;  // 12 / 3 = 4 (float 4.0)
 
         //4. 평균값을 float로 출력하기
-        System.out.println("평균: " + avg);
+        System.out.println("평균: " + avg); // 평균 : 4.0
     }
 }
 
