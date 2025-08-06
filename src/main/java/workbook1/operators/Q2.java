8df20439c61920457f17f9d3e978993b16fdec8b
ehy0419 <2hy0419@naver.com>
2025-08-06 09:08:22 +0900

    docs:[operators]Q2 해설 추가

--- src/main/java/workbook1/operators/Q2.java
+++ src/main/java/workbook1/operators/Q2.java
@@ -30,3 +30,14 @@ public class Q2 {
 // 후위 증가값
 // 출력은 '6 6' 으로 보아
 // 전위 증가값 " " 후위 증가값 출력하기
+
+// 입력값이 5 일 때, ++num은 먼저 6으로 증가시키고 그 값을 preInc에 저장.
+// nun++ 은 6을 postInc에 저장한 뒤, num을 7로 증가시킴.
+// 출력은 preInc와 증가된 num 이므로 6 7 이 될 것 같았지만
+// 문제 예시처럼 6 6으로 나왔다.
+
+// preInc : ++num -> num이 먼저 6으로 증가하고, 그 값을 preInc에 저장하니, 6
+// postInc : num++ -> 숫자 6을 pstInc에 저장하고, 그 후 num은 7로 증가하니, 6
+// 즉, postInc는 증가 전의 값을 저장하고, num 자체는 이후에 7이 됩니다.
+// 하지만 출력문 System.out.println(preInc + " " + postInc);에서
+// num이 아닌 postInc를 출력하므로 둘 다 6 이 되는 것이다.
