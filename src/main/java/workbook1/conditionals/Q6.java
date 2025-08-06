166e1bdfc75e1960e5f64857eb4336f1576a3012
ehy0419 <2hy0419@naver.com>
2025-08-06 18:29:31 +0900

    feat:[conditonals] Q6 완성 - 주석포함

--- src/main/java/workbook1/conditionals/Q6.java
+++ src/main/java/workbook1/conditionals/Q6.java
@@ -8,5 +8,19 @@ public class Q6 {
         double weight = sc.nextDouble();
         double heightCm = sc.nextDouble();
         // TODO: BMI 계산 후 조건문으로 등급 출력
+
+        // 공식 :  BMI = 몸무게 / (키/100)^2
+        double BMI = weight / ((heightCm/100) * (heightCm/100));
+
+        if (BMI >= 30) {
+            System.out.println("비만");   // BMI가 30 이상이면 “비만"
+        } else if (BMI >= 30) {
+            System.out.println("과체중"); // BMI가 25 이상이면 “과체중”
+        }  else if (BMI >= 18.5) {
+            System.out.println("정상");   // BMI가 18.5 이상이면 “정상”
+        } else {
+            System.out.println("저체중");  // BMI가 그 외는 “저체중”
+        }
+        sc.close();
     }
 }
