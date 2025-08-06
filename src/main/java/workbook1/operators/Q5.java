bd5bbc9ecfa7dcaebbabd3c10c29dfebab939cf3
ehy0419 <2hy0419@naver.com>
2025-08-06 09:32:32 +0900

    feat:[operators]Q5 완성

--- src/main/java/workbook1/operators/Q5.java
+++ src/main/java/workbook1/operators/Q5.java
@@ -5,9 +5,25 @@ import java.util.Scanner;
 public class Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
         // TODO: a += b * c 연산 후 a, b, c 출력
+        // 세 개의 정수 입력 받기
+        System.out.print("정수 a: ");
+        int a = sc.nextInt();
+        System.out.print("정수 b: ");
+        int b = sc.nextInt();
+        System.out.print("정수 c: ");
+        int c = sc.nextInt();
+
+        // 복합대입 연산자 사용 a += b * c
+        a += b * c;
+
+        // 입력 결과
+        System.out.println("결과: " + a + " " + b + " " + c);
     }
 }
+
+///문제 분석
+// 입력처리
+// 복합대입 연산자 사용 a += b * c
+// 입력 테스트
+//조건 : 원본 b, c 변경 금지!
