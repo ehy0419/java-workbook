cba0c0ac6e1bab7824a4c895814e810870510253
ehy0419 <2hy0419@naver.com>
2025-08-08 18:49:48 +0900

    feat:[controlflow] Q5 주석 미포함 완성

--- src/main/java/workbook1/controlflow/Q5.java
+++ src/main/java/workbook1/controlflow/Q5.java
@@ -7,5 +7,18 @@ public class Q5 {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         // TODO: 각 자리수의 합을 반복해 한 자리 수가 될 때까지 계산 후 출력
+        // 반복문과 조건문의 조합으로 자리수 합을 반복 계산
+
+        while (num > 10) {      // 주어진 숫자가 두 자리 이상 일 경우 반복
+            int sum = 0;        // 자리수 합 초기화
+
+            while(num > 0) {
+                sum += num % 10; // 나머지 수 sum에 합산
+                num /= 10;
+            }
+            num = sum;
+        }
+        System.out.println(num);
+        sc.close();
     }
 }
