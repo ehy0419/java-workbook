83706ee1f8283502b1c0a681a155de82c53b97e6
ehy0419 <2hy0419@naver.com>
2025-08-08 17:37:44 +0900

    feat:[loops] Q6. 세로로 출력됨

--- src/main/java/workbook1/loops/Q6.java
+++ src/main/java/workbook1/loops/Q6.java
@@ -3,5 +3,26 @@ package main.java.workbook1.loops;
 public class Q6 {
     public static void main(String[] args) {
         // TODO: 구구단 전체를 출력
+        // 2부터 9까지 구구단
+        // 세로 형태로 출력
+
+        /// 잘못된 출력 - 변수명이 문자열로 출력.
+//        for (int i = 1; i < 10; i++) {
+//            for(int j = 2; j < 10; j++) {
+//                System.out.println(j + "*" + i + "=" + (j * i));
+//            }
+//            System.out.println(); // 한 행 끝나면 줄 바꿈
+//        }
+        // 출력에서 실제 곱셈 결과가 아니라 변수명 자체인 'j * i = 2' 문자열로 출력
+        // System.out.println("j" + "*" + "i" + "=" + (j * i)); 이 부분을
+        // System.out.println(j + "*" + i + "=" + (j * i)); 으로 수정하면 좋다.
+
+        /// 올바른 출력
+        for (int x = 2; x <= 9; x++) { // 2부터 9까지의 구구단
+            for (int y = 1; y <= 9; y++) { // 각 단에서 1 ~ 9 곱셈
+                System.out.println(x + "x" + y + "=" + (x * y));
+            }
+            System.out.println(); // 단마다 줄바꿈
+        }
     }
 }
