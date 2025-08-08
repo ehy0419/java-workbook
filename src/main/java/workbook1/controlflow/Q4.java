3dabc9f3f510463219a79c107976274e57df782e
ehy0419 <2hy0419@naver.com>
2025-08-08 18:41:26 +0900

    feat:[controlflow] Q4

--- src/main/java/workbook1/controlflow/Q4.java
+++ src/main/java/workbook1/controlflow/Q4.java
@@ -6,5 +6,22 @@ public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // TODO: 0이 나올 때까지 양수만 합산하는 코드 작성
+        // while 문을 사용하고 입력값이 0이면 반복 종료.
+
+        int sum = 0;
+
+        while(true){
+            int num = sc.nextInt();
+
+            if (num > 0 ) {
+                sum += num;
+            }
+
+            if (num == 0) {
+                break;
+            }
+        }
+        System.out.println(sum);
+        sc.close();
     }
 }
