2098a326ef3443e7520a94651130ffee3cb2b6a5
ehy0419 <2hy0419@naver.com>
2025-08-08 18:11:15 +0900

    feat:[controlflow] Q1. 완성

--- src/main/java/workbook1/controlflow/Q1.java
+++ src/main/java/workbook1/controlflow/Q1.java
@@ -7,5 +7,18 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // TODO: 짝수는 합, 홀수는 곱에 반영 후 최종 결과 출력
+
+        int sum = 0; // 초기 합 : 0
+        int mul = 1; // 초기 곱 : 1
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            } else {
+                mul *= i;
+            }
+        }
+        System.out.println(sum + " " + mul);
+        sc.close();
     }
 }
