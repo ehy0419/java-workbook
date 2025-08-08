e8bfbe24f708354aeadf632bb7510daf1b3f26e8
ehy0419 <2hy0419@naver.com>
2025-08-08 18:30:19 +0900

    bug:[controlflow] Q3. 소수 판별 논리에서 막힘...

--- src/main/java/workbook1/controlflow/Q3.java
+++ src/main/java/workbook1/controlflow/Q3.java
@@ -7,5 +7,20 @@ public class Q3 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // TODO: 1~n까지의 소수를 찾아 출력
+
+        //소수 : 1과 자신 만을 약수로 갖는 수
+
+        for (int i = 2; i <= n; i++) {
+            boolean rightPirme = true;      // 소수로 인정이 되면
+
+            for  (int j = 2; j <= n; j++) {
+                if (i % j == 0) {
+                    rightPirme = false; break;
+                }
+            }
+            if (rightPirme) {
+                System.out.println(i + " ");
+            }
+        }
     }
 }
