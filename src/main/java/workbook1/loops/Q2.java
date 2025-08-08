aca34a7f59517600c6b27c1bedfea2d68269544b
ehy0419 <2hy0419@naver.com>
2025-08-08 13:16:06 +0900

    feat:[loops] Q2 n=5에서 안됨.. 수정완료

--- src/main/java/workbook1/loops/Q2.java
+++ src/main/java/workbook1/loops/Q2.java
@@ -10,15 +10,30 @@ public class Q2 {
         int n = sc.nextInt();
         // TODO: 홀수 합과 짝수 합 계산 후 출력
 
-        // 수미상관?에 맞춰서
+        /// 수미상관?에 맞춰서
+        // 입력 - 처리 - 출력
+        // 조건 - 합산 - 출력
+        // 짝수의 합 - 짝수 조건식
+        // 홀수의 합 - 홀수 조건식
+
         int evenSum = 0;    // 짝수의 합
         int oddSum = 0;     // 홀수의 합
 
-        for (int i = 2; i <= n; i++) {  // 입력된 n의 짝수의 합 계산
-            evenSum += i*2;
-        }
-        for (int i = 1; i <= n; i++) {  // 입력된 n의 홀수의 합 계산
-            oddSum += i;
+        // 오류 : n= 5 일 때, 2+4 가 맞음
+        // 아래 식에서는 4 + 6 + 8 + 10이 되버림..
+//        for (int i = 2; i <= n; i++) {  // 입력된 n의 짝수의 합 계산
+//            evenSum += i*2;
+//        }
+//        for (int i = 1; i <= n; i++) {  // 입력된 n의 홀수의 합 계산
+//            oddSum += i;
+//        }
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {   // 짝수 계산 - 나머지 0이 되면 짝수
+                evenSum += i;
+            } else  {           // 홀수 계산 나머지 1이 되면 홀수
+                oddSum += i;
+            }
         }
 
         System.out.println("짝수의 합: " + evenSum);
