4241ccc7ea66ed59a5cc86bd27a458a1508c74b4
ehy0419 <2hy0419@naver.com>
2025-08-08 13:32:31 +0900

    test:[loops] Q3 while, dowhile 비교 테스트문

--- src/main/java/workbook1/loops/Q3.java
+++ src/main/java/workbook1/loops/Q3.java
@@ -15,12 +15,22 @@ public class Q3 {
         //while - 조건식이 참 일 때 코드가 실행되는 것
         //do while - 무조건 한 번 이상 실행되는 것
 
-        ///  테스트
-        int num = 6;
+
+        // 링크 : https://nirsa.tistory.com/160
+        ///  테스트1
+        int num1 = 6;           // 변수에 6할당
         do {
-            System.out.println("6이에용");
-            num++;
-        } while (num <= 5);
-        System.out.println("작동하나요?");
+            System.out.println(num1);
+            num1++;                     // 6
+        } while (num1 <= 5);
+        System.out.println("작동하나요?");   // 작동하나요?
+
+        ///  테스트2
+        int num2 = 4;               // 1. 변수에 4 할당
+        do {                                        // 5. 돌아와서 num +1 = 5
+            System.out.println(num2);   // 2. num = 4 출력
+            num2++;             // 3. num = 5       // 6. num = 6
+        } while (num2 <= 5);    // 4. while 참      // 7. while 거짓
+        System.out.println("작동하나요?");   // 작동하나요?
     }
 }
